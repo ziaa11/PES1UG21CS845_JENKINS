095d61b754983308fb63e1d12bc166730081a89a
Zia Ur Rehman <ziaprivate11@gmail.com>
2023-02-16 05:02:41 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run PES1UG21CS845\n";
+  cout << "I have successfully built and run PES1UG21CS845 making change\n";
   return 0;
 }
