a5d54806d0cc5c27056fdce82f88aad924741a63
Zia Ur Rehman <ziaprivate11@gmail.com>
2023-02-17 09:24:29 +0530

    changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void)
 {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run PES1UG21CS845 making change\n";
+  cout << "I have successfully built and run PES1UG21CS845 (making change)\n";
   return 0;
 }
